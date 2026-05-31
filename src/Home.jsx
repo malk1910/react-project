cee5e9b2113394722dc93f932cff9c492284323f
malk1910 <engmalkayman2005@gmail.com>
2026-05-31 18:07:26 +0300

    update project2

--- src/Home.jsx
+++ src/Home.jsx
@@ -25,9 +25,9 @@ export default function Home(){
    <button onClick={inc}>inc number</button> */}
    <div className="bg-secondary-subtle min-vh-100 lh-lg d-flex justify-content-center align-items-center">
    <div className="container text-center pt-5">
-    <p className="fw-bold text-danger-emphasis ">NEXT-GEN EXPERIENCE STUDIO</p>
-    <h1 className="fw-bold">Elevate brands with <span className="text-danger-emphasis">high-impact </span>design.</h1>
-    <p className="text-dark-emphasis">We combine strategy, creativity and design to craft digital experiences that inspire action and fuel growth.</p>
+    <p className="fw-bold text-danger-emphasis "> ducimus porro facilis vero dolorem</p>
+    <h1 className="fw-bold">Learning <span className="text-danger-emphasis">REACT </span>.</h1>
+    <p className="text-dark-emphasis">Lorem ipsum dolor sit Laborum iste suscipit impedit aut quas commodi nulla.</p>
     <button className="btn bg-dark  text-white">Let's talk</button>
     
    </div>
